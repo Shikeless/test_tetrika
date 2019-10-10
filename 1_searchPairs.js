d2b1da836529a1135971d3656fc63cf6135285de
Shikeless <Shikeless@gmail.com>
2019-10-10 22:52:13 +0300

    adding comments

--- 1_searchPairs.js
+++ 1_searchPairs.js
@@ -1,11 +1,15 @@
 // Дан массив целых чисел <array> и целое число <k>. Нужно вывести все уникальные пары чисел из массива, сумма которых равна k.
 // OUT: >> [[1, 4], [2, 3]]
 
+// аргумент написан с большой буквы, опечатка?
 function searchPairs(array, N) {
+    // удаляем повторяющиеся значения, при необходимости
     const filtered = array.filter(
         (item, index) => array.indexOf(item) === index
     );
+
     var result = [];
+
     filtered.forEach((item, index) => {
         for (i = index + 1; i < filtered.length; i++) {
             if (item + filtered[i] == N) result.push([item, filtered[i]]);
@@ -15,6 +19,7 @@ function searchPairs(array, N) {
 }
 
 const arr = [1, 2, 6, 5, 3, 4, 7, 8, 1, 4];
+
 const k = 5;
 
 console.log(searchPairs(arr, k));
